28fd914de1fe7e9e26433bcadaa604a2fd60781f
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2025-01-29 18:19:42 -0500

    Update test-svc-1.py test3

--- source/test-svc-1.py
+++ source/test-svc-1.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     retVal = {
-        "msg":"hello world part 2",
+        "msg":"hello world part 3",
         "host":"%s" % socket.gethostname()
 
     }
